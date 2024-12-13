fbe4a4f09a36fe8fc72182aaaef376797925e19a
salix <csauce295@gmail.com>
2024-12-12 21:13:32 -0600

    project completed

--- frontend/components/Quotes.js
+++ frontend/components/Quotes.js
@@ -4,36 +4,45 @@ import {
   setHighlightedQuote,
   toggleVisibility,
 } from '../state/quotesSlice'
+import { useGetQuotesQuery, useToggleFakeMutation, useDeleteQuoteMutation } from '../state/quotesApi'
 
 export default function Quotes() {
-  const quotes = [
-    {
-      id: 1,
-      quoteText: "Don't cry because it's over, smile because it happened.",
-      authorName: "Dr. Seuss",
-      apocryphal: true,
-    },
-    {
-      id: 2,
-      quoteText: "So many books, so little time.",
-      authorName: "Frank Zappa",
-      apocryphal: false,
-    },
-    {
-      id: 3,
-      quoteText: "Be yourself; everyone else is already taken.",
-      authorName: "Oscar Wilde",
-      apocryphal: false,
-    },
-  ]
+  const { data: quotes, isLoading: quotesLoading, isFetching: quotesRefreshing } = useGetQuotesQuery()
+  const [ toggleFake, {isLoading: togglingQuote} ] = useToggleFakeMutation()
+  const [ deleteQuote, {isLoading: deletingQuote }] = useDeleteQuoteMutation()
+  // const quotes = [
+  //   {
+  //     id: 1,
+  //     quoteText: "Don't cry because it's over, smile because it happened.",
+  //     authorName: "Dr. Seuss",
+  //     apocryphal: true,
+  //   },
+  //   {
+  //     id: 2,
+  //     quoteText: "So many books, so little time.",
+  //     authorName: "Frank Zappa",
+  //     apocryphal: false,
+  //   },
+  //   {
+  //     id: 3,
+  //     quoteText: "Be yourself; everyone else is already taken.",
+  //     authorName: "Oscar Wilde",
+  //     apocryphal: false,
+  //   },
+  // ]
   const displayAllQuotes = useSelector(st => st.quotesState.displayAllQuotes)
   const highlightedQuote = useSelector(st => st.quotesState.highlightedQuote)
   const dispatch = useDispatch()
   return (
     <div id="quotes">
-      <h3>Quotes</h3>
+      <h3>Quotes
+          {deletingQuote && ' in deletion...'}
+          {togglingQuote && ' being toggled...'}
+          {(quotesLoading || quotesRefreshing) && ' loading....'}
+      </h3>
       <div>
         {
+          quotesLoading ? 'quotes loading...' :
           quotes?.filter(qt => {
             return displayAllQuotes || !qt.apocryphal
           })
@@ -45,9 +54,9 @@ export default function Quotes() {
                 <div>{qt.quoteText}</div>
                 <div>{qt.authorName}</div>
                 <div className="quote-buttons">
-                  <button>DELETE</button>
+                  <button onClick={() => deleteQuote(qt.id)}>DELETE</button>
                   <button onClick={() => dispatch(setHighlightedQuote(qt.id))}>HIGHLIGHT</button>
-                  <button>FAKE</button>
+                  <button onClick={() => toggleFake({id: qt.id, quote: { apocryphal: !qt.apocryphal} })}> FAKE</button>
                 </div>
               </div>
             ))
--- frontend/state/quotesApi.js
+++ frontend/state/quotesApi.js
@@ -1,1 +1,44 @@
 // create your RTK Query endpoints here
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const quotesApi = createApi({
+    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:9009/api/' }),
+    tagTypes: ['Quotes'],
+    endpoints: builder => ({
+        getQuotes: builder.query({
+            query: () => '/quotes', // Define endpoint
+            providesTags: ['Quotes'],
+        }),
+        createQuote: builder.mutation({
+            query: (newQuote) => ({
+                url: '/quotes',
+                method: 'POST',
+                body: newQuote,
+            }),
+            invalidatesTags: ['Quotes'],
+        }),
+        toggleFake: builder.mutation({
+            query: ({id, quote}) => ({
+                url: `quotes/${id}`,
+                method: 'PUT',
+                body: quote
+            }),
+            invalidatesTags: ['Quotes'],
+        }),
+        deleteQuote: builder.mutation({
+            query: (id) => ({
+                url: `/quotes/${id}`,
+                method: 'DELETE',
+            }),
+            invalidatesTags: ['Quotes'],
+        }),
+
+    })
+})
+
+export const {
+    useGetQuotesQuery,
+    useCreateQuoteMutation,
+    useToggleFakeMutation,
+    useDeleteQuoteMutation,
+} = quotesApi
